a6003a54cea280d0040843112fc44b79113e10bc
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-03-22 22:19:07 +0000

    Hard coded values for time being

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-fetch('./data.json')
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
+// fetch('data.json')
+//   .then((res) => res.json())
+//   .then((data) => {
+//     const h3 = document.querySelector('.summary-item-title');
+//     h3.textContent = data;
+//   })
+//   .catch((error) => console.error(error));
